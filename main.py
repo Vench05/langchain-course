5c1d68861938e5ff1a43f00dc6abef8fab904697
Ralf Vench <ralfvenchbillones@gmail.com>
2025-12-30 17:35:55 +0800

    Added schemas with pydantic and manual prompt template

--- main.py
+++ main.py
@@ -5,30 +5,21 @@ from pydantic import BaseModel, Field
 
 load_dotenv()
 
-from langchain.agents import AgentState
-from langchain.agents import create_agent
+from langchain.agents import AgentState, create_agent
 from langchain.tools import tool
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-from langchain_classic import hub
-from langchain_classic.agents import AgentExecutor
-from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 
-class Source(BaseModel):
-    """Schema for a source used by the agent."""
-
-    url: str = Field(description="The URL of the source.")
-
-
-class AgentResponse(BaseModel):
-    """Schema for the agent response."""
-
-    answer: str = Field(description="The Agent's answer to the user query.")
-    sources: List[Source] = Field(
-        default_factory=list, description="List of sources used to generate the answer."
-    )
+from prompt import REACT_PROPMT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 
 
@@ -36,8 +27,13 @@ def main():
     print("Hello from langchain-course!")
     llm = ChatOpenAI(model="gpt-4", temperature=0)
     tools = [TavilySearch()]
-    react_propmt = hub.pull('hwchase17/react')
-    agent = create_react_agent(llm=llm, tools=tools, prompt=react_propmt)
+    react_propmt = hub.pull("hwchase17/react")
+    output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+    react_propmpt_with_format_instructions = PromptTemplate(
+        template=REACT_PROPMT_WITH_FORMAT_INSTRUCTIONS,
+        input_variables=["input", "agent_scratchpad", "tool_names", "tools"],
+        ).partial(format_instructions=output_parser.get_format_instructions())
+    agent = create_react_agent(llm=llm, tools=tools, prompt=react_propmpt_with_format_instructions)
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
     chain = agent_executor
 
